937a15b76147f870a115c75c66e8086a61bb3a0f
Mohammadreza <mr.ebrahimipur>
2024-06-25 15:06:36 +0330

    [imp] Error handling

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,19 +1,48 @@
 <?php
 
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpFoundation\Response as HttpResponse;
+
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->renderable(function (ModelNotFoundException $e) {
+            return response()->json([
+                'message' => $e->getMessage(),
+            ], HttpResponse::HTTP_NOT_FOUND);
+        });
+        $exceptions->renderable(function (NotFoundHttpException $e) {
+            return response()->json([
+                'message' => 'Not Found',
+            ], $e->getStatusCode());
+        });
+        $exceptions->renderable(function (MethodNotAllowedHttpException $e) {
+            return response()->json([
+                'message' => 'Not Found',
+            ], $e->getStatusCode());
+        });
+        $exceptions->renderable(function (MethodNotAllowedHttpException $e) {
+            return response()->json([
+                'message' => 'Not Found',
+            ], $e->getStatusCode());
+        });
+        $exceptions->renderable(function (\Exception $e) {
+            return response()->json([
+                'message' => 'error',
+            ], HttpResponse::HTTP_NOT_FOUND);
+        });
     })->create();
